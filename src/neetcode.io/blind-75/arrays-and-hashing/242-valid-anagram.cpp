547262efd41db3a6df8258c9ff6104cb4f89f5b1
Vincentius Lienardo <vincentiuslienardo@gmail.com>
2023-01-20 19:18:54 +0700

    docs(leetcode): add explanation to 242-valid-anagram

--- src/neetcode.io/blind-75/arrays-and-hashing/242-valid-anagram.cpp
+++ src/neetcode.io/blind-75/arrays-and-hashing/242-valid-anagram.cpp
@@ -6,7 +6,8 @@ class Solution {
  public:
   bool isAnagram(string s, string t) {
     if (s.size() != t.size()) return false;
-    unordered_map<char, int> m;
+    unordered_map<char, int> m;  // Use unordered map for Unicode, vector of
+                                 // size 26 for lowercase.
     for (char& c : s) ++m[c];
     for (char& c : t)
       if (--m[c] < 0) return false;
